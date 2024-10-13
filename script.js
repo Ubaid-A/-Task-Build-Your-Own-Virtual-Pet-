ad38d5fcf8d0bbc0ac4d3ad423985a4e0fec3914
Ubaid-A <wafaubaid3@gmail.com>
2024-10-13 10:36:39 +0500

    use prompt to get data from user

---
+++ script.js
@@ -0,0 +1,4 @@
+var petName = prompt("Enter your pet's name:");
+var petType = prompt("What type of pet is it? (e.g., dog, cat, etc.):");
+var petAge = Number(prompt("Enter your pet's age:"));
+    
